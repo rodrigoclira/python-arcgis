241bca498c957eddfc18acc02adae10c7477c0f7
rodrigocliras@gmail.com <rodrigocliras@gmail.com@d770449e-75b2-ed60-5da0-07c3a175e4f9>
2013-01-05 02:28:57 +0000

    Exemplo de como pegar o lote de acordo com o centroide da edificação.

---
+++ search_lote.py
@@ -0,0 +1,20 @@
+#Procura o lote da edificacao
+
+import arcpy
+cur_edificacao  = arcpy.SearchCursor('RFPM_Edificacoes_dwg_Polygon')
+cur_lote = arcpy.SearchCursor('RFPM_Lotes')
+flag = False
+for edificacao in cur_edificacao:
+    if int(row.getValue('Elevation')) == 103012:
+	poligono = row.getValue('Shape')
+        centroide = poligono.centroid
+        if flag:
+            break
+	for lote in cur_lote:
+            pol_lote = lote.getValue("Shape")
+            if pol_lote.contains(centroide):
+                print "FID do LOTE", lote.getValue("FID")
+                print "Area", pol_lote.area
+                flag = True
+                break
+            
